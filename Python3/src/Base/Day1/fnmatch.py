4c46d913d16d13940dde7c35805c9ad4a7a8efc3
yushanyong <yushanyong520@hotmail.com>
2016-03-29 00:36:41 +0800

    first

--- Python3/src/Base/Day1/fnmatch.py
+++
@@ -1,5 +0,0 @@
-#coding:UTF-8
-# 使用Unix Shell中常用的通配符(比如*.py,Dat[0-9]*.csv等)去匹配文本字符串
-# fnmatch 模块提供了两个函数—fnmatch()和fnmatchcase()用来实现匹配
-from fnmatch import fnmatch
-print(fnmatch('foo.txt', '*.txt'))
