89b82c25bff9877889ac2f0059a2d63e2b98082f
yushanyong <yushanyong520@hotmail.com>
2016-04-01 13:15:04 +0800

    five

--- src/Base/Day2_Digit_Time_Date/complex_oper.py
+++ src/Base/Day2_Digit_Time_Date/complex_oper.py
@@ -17,4 +17,27 @@ print(cmath.sqrt(-1))
 from fractions import Fraction
 frac = Fraction(5,4)
 frac1 = Fraction(7,16)
-print(a + 6)
+print(frac + frac1)
+print(frac * frac1)
+print(float(frac*frac1))
+import numpy as np
+ax = np.array([1,2,3,4])
+print (ax*2)
+ay = np.array([5,6,7,8])
+print (ay*2)
+print (ax + 10)
+print (ax + ay)
+print (ax * ay)
+def f(x):
+    return 3*x**2 - 2*x +7
+print (f(ax))
+
+import random
+values= [1,2,3,4,5,6]
+print (random.choice(values))#随机提取一个
+print (random.sample(values,3))#随机提取3个
+random.shuffle(values)
+print (values)
+print (random.randint(0,10))#生成随机数
+print (random.random())#生成0到1之间均匀分布的浮点数
+print (random.getrandbits(20))#获取N位随机（二进制）的整数
